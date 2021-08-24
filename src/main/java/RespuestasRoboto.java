06531d423c5ae117d7411f50ccb3470f18870d83
Marialauramn16 <hikco25@gmail.com>
2021-08-24 10:00:50 +0200

    creando test 1 y test 2 de kata robotto

--- src/main/java/RespuestasRoboto.java
+++ src/main/java/RespuestasRoboto.java
@@ -1,8 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RespuestasRoboto {
     public String respuesta (String frase){
-        if (saludo === ("Hola"+"Roboto")) {
-            return ("")
+        Pattern pat = Pattern.compile("¿Que tiempo hace en (\\w+)\\?");
+        Matcher mat = pat.matcher(frase);
+          if (mat.find()) {
+              return ("Te vas a " + mat.group(1) + " y lo miras");
+          }
+            return "Hola";
         }
-    }
 
 }
+
+
+
--- src/test/java/RobotoTest.java
+++ src/test/java/RobotoTest.java
@@ -1,10 +1,25 @@
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class RobotoTest {
+
     @Test
     void cuandoSaludasTeDevuelveElSaludo(){
         RespuestasRoboto saludo = new RespuestasRoboto();
         String respuesta = saludo.respuesta("Hola, Roboto");
         assertThat(respuesta, equalTo("Hola"));
     }
+
+    @Test
+    void cuandoPreguntoElTiempoMeDevuelveElTiempo(){
+        RespuestasRoboto tiempo = new RespuestasRoboto();
+        String respuesta = tiempo.respuesta( "¿Que tiempo hace en Barcelona?");
+        assertThat(respuesta, equalTo("Te vas a Barcelona y lo miras"));
+    }
+
+    @Test
+    void cuandoLe
+
 }
